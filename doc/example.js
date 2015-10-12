13c8f75cb734fc31b60286b2d36ea6536e44c668
Rob Huzzey <robert.huzzey@holidayextras.com>
2015-10-12 10:47:36 +0100

    The first PR

---
+++ doc/example.js
@@ -0,0 +1,39 @@
+'use strict';
+/* eslint-disable no-alert */
+
+var React = require('react');
+var ProgressBar = require('../src/ProgressBar.jsx');
+
+var Example = React.createClass({
+
+  getInitialState: function() {
+    return {
+      loadedTime: 0
+    };
+  },
+
+  updateTime: function() {
+    this.setState({ loadedTime: this.state.loadedTime + 10 });
+  },
+
+  render: function() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1>Progress Bar</h1>
+        </div>
+
+        <h2>Without external control of loading</h2>
+        <h3>Defaults to 100% loaded</h3>
+        <ProgressBar />
+
+        <h2>With an external control</h2>
+        <h3>Updates based on a percent (as integer) you pass in</h3>
+        <ProgressBar now={this.state.loadedTime} />
+        <button className="btn btn-default" onClick={this.updateTime}>Load 10% more</button>
+      </div>
+    );
+  }
+});
+
+React.render(<Example />, document.getElementById('container'));
---
+++ test/testProgressBar.jsx
@@ -0,0 +1,49 @@
+'use strict';
+
+var React = require('react/addons');
+var ReactBootstrap = require('react-bootstrap');
+var TestUtils = React.addons.TestUtils;
+
+var assert = require('assert');
+
+var ProgressBar = require('../src/ProgressBar');
+
+var shallowRender = function(jsx) {
+  var shallowRenderer = TestUtils.createRenderer();
+  shallowRenderer.render(jsx);
+  return shallowRenderer.getRenderOutput();
+};
+
+var defaultProps = {
+  now: 100,
+  title: null
+};
+
+// No need for too many tests here as we are using react bootstrap library
+// so we can rely on the tests for that library covering us.
+describe('ProgressBar', function() {
+
+  describe('with no props passed', function() {
+    it('is an element', function() {
+      assert(TestUtils.isElement(<ProgressBar />));
+    });
+
+    it('has default values for many props', function() {
+      var element = <ProgressBar />;
+      assert.deepEqual(element.props, defaultProps);
+    });
+
+    it('returns a react-bootstrap panel', function() {
+      var renderOutput = shallowRender(<ProgressBar />);
+      assert.equal(renderOutput.type, ReactBootstrap.Panel);
+    });
+
+    it('contains a single react progressbar inside the panel', function() {
+      var renderOutput = shallowRender(<ProgressBar />);
+      var child = React.Children.only(renderOutput.props.children);
+      assert.equal(child.type, ReactBootstrap.ProgressBar);
+    });
+
+  });
+
+});
