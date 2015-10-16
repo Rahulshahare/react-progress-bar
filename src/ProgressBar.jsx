7ff7d538bb4676fa121cdf91e2b5d2ff5e197b0e
Rob Huzzey <robert.huzzey@holidayextras.com>
2015-10-16 11:49:48 +0100

    Got coverage back to 100%

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -7,7 +7,9 @@ var ProgressBar = React.createClass({
 
   propTypes: {
     now: React.PropTypes.number,
+    duration: React.PropTypes.number,
     title: React.PropTypes.string,
+    subtitle: React.PropTypes.string,
     type: React.PropTypes.oneOf(['success', 'info', 'warning', 'danger']),
     children: React.PropTypes.node
   },
@@ -17,6 +19,7 @@ var ProgressBar = React.createClass({
       now: 100,
       duration: 1,
       title: null,
+      subtitle: null,
       type: 'info'
     };
   },
@@ -24,30 +27,34 @@ var ProgressBar = React.createClass({
   getInitialState: function() {
     return {
       loaded: 0
-    }
+    };
   },
 
   componentDidMount: function() {
     var self = this;
     setTimeout(function() {
-      self.setState({loaded: self.props.now});
+      self.setState({ loaded: self.props.now });
     }, 500);
   },
 
   componentWillReceiveProps: function(nextProps) {
-    if(nextProps.now !== this.props.now) {
-      this.setState({loaded: nextProps.now});
+    if (nextProps.now !== this.props.now) {
+      this.setState({ loaded: nextProps.now });
     }
   },
 
   render: function() {
     var timing = this.props.duration + 's';
+    var subtitle = this.props.subtitle ? <p>{this.props.subtitle}</p> : undefined;
+
+    // TODO: We need a better way of handling the CSS transitions here.
     var uniqueClass = 'progressBar' + timing;
     var style = '.' + uniqueClass + ' {transition-duration: ' + timing + '; }';
-    
+
     return (
       <ReactBootstrap.Panel header={this.props.title} bsStyle={this.props.type}>
         <style>{style}</style>
+        {subtitle}
         <ReactBootstrap.ProgressBar className={uniqueClass} active striped bsStyle={this.props.type} now={this.state.loaded} />
         {this.props.children}
       </ReactBootstrap.Panel>
--- test/testProgressBar.jsx
+++ test/testProgressBar.jsx
@@ -1,22 +1,24 @@
 'use strict';
 
+require('react-tests-globals-setup');
 var React = require('react/addons');
 var ReactBootstrap = require('react-bootstrap');
 var shallowRender = require('react-shallow-render');
 var TestUtils = React.addons.TestUtils;
 
 var assert = require('assert');
+var sinon = require('sinon');
 
 var ProgressBar = require('../src/ProgressBar');
 
 var defaultProps = {
   now: 100,
+  duration: 1,
   title: null,
+  subtitle: null,
   type: 'info'
 };
 
-// No need for too many tests here as we are using react bootstrap library
-// so we can rely on the tests for that library covering us.
 describe('ProgressBar', function() {
 
   it('is an element', function() {
@@ -28,7 +30,7 @@ describe('ProgressBar', function() {
     assert.deepEqual(element.props, defaultProps);
   });
 
-  context('with no children passed in', function() {
+  context('with no properties', function() {
 
     var renderOutput;
     before(function() {
@@ -39,57 +41,106 @@ describe('ProgressBar', function() {
       assert.equal(renderOutput.type, ReactBootstrap.Panel);
     });
 
-    it('should count 2 children', function() {
-      assert.equal(React.Children.count(renderOutput.props.children), 2);
+    it('should count 4 children', function() {
+      assert.equal(React.Children.count(renderOutput.props.children), 4);
     });
 
-    context('1st child (progress bar)', function() {
+    context('1st child', function() {
+      it('should be a style tag', function() {
+        assert.equal(renderOutput.props.children[0].type, 'style');
+      });
+    });
+
+    context('3rd child', function() {
       it('should be a ReactBootstrap ProgressBar', function() {
-        assert.equal(renderOutput.props.children[0].type, ReactBootstrap.ProgressBar);
+        assert.equal(renderOutput.props.children[2].type, ReactBootstrap.ProgressBar);
       });
 
       it('should have the same bsStyle prop as the parent', function() {
-        assert.equal(renderOutput.props.children[0].props.bsStyle, renderOutput.props.bsStyle);
+        assert.equal(renderOutput.props.children[2].props.bsStyle, renderOutput.props.bsStyle);
       });
     });
 
-    context('2nd child (this.props.children property)', function() {
+    context('2nd & 4th child', function() {
       it('should be undefined', function() {
+        assert.equal(renderOutput.props.children[3], undefined);
         assert.equal(renderOutput.props.children[1], undefined);
       });
     });
 
   });
 
-  context('with a child passed in', function() {
+  context('with a title', function() {
+    it('should have the title set on the panel', function() {
+      var title = 'This is a title';
+      var renderOutput = shallowRender(<ProgressBar title={title} />);
+      assert.equal(renderOutput.props.header, title);
+    });
+  });
+
+  context('with a subtitle', function() {
 
     var renderOutput;
+    var subtitle = 'This is a subtitle';
     before(function() {
-      renderOutput = shallowRender(<ProgressBar><h1>Foobar</h1></ProgressBar>);
+      renderOutput = shallowRender(<ProgressBar subtitle={subtitle} />);
     });
 
-    it('returns a react-bootstrap panel', function() {
-      assert.equal(renderOutput.type, ReactBootstrap.Panel);
+    it('should have a 2nd child that is a paragraph', function() {
+      assert.equal(renderOutput.props.children[1].type, 'p');
     });
 
-    it('should count 2 children', function() {
-      assert.equal(React.Children.count(renderOutput.props.children), 2);
+    it('should have the correct text', function() {
+      assert.equal(renderOutput.props.children[1].props.children, subtitle);
+    });
+  });
+
+  context('render into document', function() {
+
+    var testParent, element, clock, now;
+    before(function() {
+      clock = sinon.useFakeTimers();
+      now = 75;
+      testParent = React.createFactory(React.createClass({
+        getInitialState: function() {
+          return { testState: now };
+        },
+        render: function() {
+          return <ProgressBar ref="sot" now={this.state.testState} />;
+        }
+      }));
+
+      element = TestUtils.renderIntoDocument(testParent());
     });
 
-    context('1st child (progress bar)', function() {
-      it('should be a ReactBootstrap ProgressBar', function() {
-        assert.equal(renderOutput.props.children[0].type, ReactBootstrap.ProgressBar);
+    after(function() {
+      clock.restore();
+    });
+
+    it('should be an instance of ProgressBar', function() {
+      assert(TestUtils.isCompositeComponentWithType(element.refs.sot, ProgressBar));
+    });
+
+    it('should move the bar ahead', function() {
+      clock.tick(500);
+      var progressBars = TestUtils.scryRenderedComponentsWithType(element.refs.sot, ReactBootstrap.ProgressBar);
+      assert.equal(progressBars[0].props.now, now);
+    });
+
+    it('should not move the bar ahead if prop passed in is the same as current prop', function() {
+      element.setState({
+        testState: now
       });
-
-      it('should have the same bsStyle prop as the parent', function() {
-        assert.equal(renderOutput.props.children[0].props.bsStyle, renderOutput.props.bsStyle);
-      });
+      var progressBars = TestUtils.scryRenderedComponentsWithType(element.refs.sot, ReactBootstrap.ProgressBar);
+      assert.equal(progressBars[0].props.now, now);
     });
 
-    context('2nd child (this.props.children property)', function() {
-      it('should be a h1', function() {
-        assert.equal(renderOutput.props.children[1].type, 'h1');
+    it('should move the bar ahead to 100 if prop changed', function() {
+      element.setState({
+        testState: 100
       });
+      var progressBars = TestUtils.scryRenderedComponentsWithType(element.refs.sot, ReactBootstrap.ProgressBar);
+      assert.equal(progressBars[0].props.now, 100);
     });
 
   });
